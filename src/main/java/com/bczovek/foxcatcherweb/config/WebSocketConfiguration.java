524b3a7fc088756fea13fab67b8014aa499483e1
bczovek <czovekbence01@gmail.com>
2021-08-14 15:11:00 +0200

    initial commit to GitHub

--- src/main/java/com/bczovek/foxcatcherweb/config/WebSocketConfiguration.java
+++ src/main/java/com/bczovek/foxcatcherweb/config/WebSocketConfiguration.java
@@ -1,15 +1,10 @@
 package com.bczovek.foxcatcherweb.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.handler.invocation.HandlerMethodReturnValueHandler;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-
-import java.util.List;
 
 @Configuration
 @EnableWebSocketMessageBroker
